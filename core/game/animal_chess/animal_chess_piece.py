18fc1debc232c87d1229ed39b3653905c0249272
Adam Chiu <zyx112388@hotmail.com>
2020-04-21 21:12:17 -0700

    reorganize folder structure

--- core/game/animal_chess/animal_chess_piece.py
+++ core/game/animal_chess/animal_chess_piece.py
@@ -1,9 +1,5 @@
+from core.game.piece import PieceCollection, Piece
 from core.game.player import Player
-
-
-class PieceCollection:
-    def __init__(self):
-        self.pieces = None
 
 
 class AnimalChessPieceCollection(PieceCollection):
@@ -22,39 +18,6 @@ class AnimalChessPieceCollection(PieceCollection):
 
     def remove_piece_on_hand(self, card):
         self.pieces.remove(card)
-
-
-class Piece:
-    # Status 0  Hidden
-    # Status 1  Shown
-
-    def __init__(self, player):
-        self.animal = None
-        self.status = 0
-        self.player = player
-        self.x = None
-        self.y = None
-
-    def flip(self):
-        if self.status is 0:
-            self.status = 1
-
-    def move(self, dest_x, dest_y):
-        pass
-
-    def set_player(self, player):
-        self.player = player
-
-    def get_status(self):
-        return self.status
-
-    def __str__(self):
-        if self.status == 0:
-            return "#####"
-        return "{}:{}".format(self.player, self.animal)
-
-    def __repr__(self):
-        return "{}:{}".format(self.player, self.animal)
 
 
 class AnimalChessPiece(Piece):
